9657bba0dde6732c213827bc510ef342a66ba559
swafey karanja <kswafey@gmail.com>
2024-11-29 16:50:11 +0300

    angular signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { AppendPipe } from './pipes/append.pipe';
 import { DataService } from './services/data.service';
 import { PhotoService } from './services/photo.service';
-import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Person } from './models/person';
 import { Posts } from './models/posts';
 import { Photos } from './models/photos';
 import { UserService } from './services/user.service';
 import { Observable } from 'rxjs';
+import { Todo } from './models/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [ AppendPipe, NgFor, NgIf, AsyncPipe, JsonPipe],
+  imports: [ AppendPipe, NgFor, NgIf, AsyncPipe],
   providers: [ DataService, PhotoService, UserService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -60,5 +61,30 @@ export class AppComponent {
   //
   // Angular signals //
 
+  todos = signal<Todo[]>([]);
+  newTodoText = signal<string>('');
+
+  handleInput(event: Event) {
+    const input = event?.target as HTMLInputElement;
+    this.newTodoText.set(input?.value);
+  }
+
+  addTodo() {
+    if(this.newTodoText().trim().length) {
+      const newTodoItem : Todo = {
+        id: Date.now(),
+        text : this.newTodoText(),
+        completed: false
+      }
+      this.todos.set([...this.todos(), newTodoItem]);
+      this.newTodoText.set('');
+    }
+  }
+
+
+  deleteTodo(id: number) {
+    const updatedTodoList = this.todos().filter((todo) => todo.id !== id);
+    this.todos.set(updatedTodoList);
+  }
 
 }
